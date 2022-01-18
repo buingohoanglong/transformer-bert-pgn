58cde9fab14b5e74b7b59bebcf42cf616fb72de1
Bui Ngo Hoang Long <buingohoanglong2000@gmail.com>
2022-01-18 08:42:23 +0700

    add dict for new data

--- build_dictionary.py
+++ build_dictionary.py
@@ -6,10 +6,10 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base", use_fast=False)
     rdrsegmenter = VnCoreNLP(address="http://127.0.0.1", port=9000) 
     dictionary = Dictionary(tokenizer=tokenizer)
-    build_vocab(dictionary=dictionary, segmenter=rdrsegmenter, file_path='data/vi-ba/train-khoi.vi')
-    build_vocab(dictionary=dictionary, segmenter=rdrsegmenter, file_path='data/vi-ba/train-khoi.ba')
+    build_vocab(dictionary=dictionary, segmenter=rdrsegmenter, file_path='data/vi-ba/train.vi')
+    build_vocab(dictionary=dictionary, segmenter=rdrsegmenter, file_path='data/vi-ba/train.ba')
     dictionary.build_dictionary()
-    dictionary.save_vocabulary('./data/vi-ba/dict-khoi.txt')
+    dictionary.save_vocabulary('./data/vi-ba/dict.txt')
 
 if __name__ == '__main__':
     main()
